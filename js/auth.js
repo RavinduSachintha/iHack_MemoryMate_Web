26cf1d486a3d4cad01ac5c2b98198b946b18a459
Ashera Silva <asherasilva100@gmail.com>
2018-10-06 17:01:05 +0530

    update repo

--- js/auth.js
+++ js/auth.js
@@ -25,7 +25,12 @@ var register = function(username,password) {
     var email = document.getElementById("email");
     var password = document.getElementById("password");
 
-    firebase.auth().createUserWithEmailAndPassword(email.value,password.value).catch(function(error) {
+    firebase.auth().createUserWithEmailAndPassword(email.value,password.value).then(function() {
+
+        // redirect to app page
+        window.location.href = "./app.html";
+
+    }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
